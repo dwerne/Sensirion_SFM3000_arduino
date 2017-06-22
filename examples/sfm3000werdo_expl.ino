04a8b53f5369ff5f9f71a05cc1a9480c84b1b323
dwerne <dwerne@ethz.ch>
2017-06-22 15:52:57 +0200

    V01 - first example
    Attention: Scaling of the sensor is solved outside the library (see line 6/7/25)!

---
+++ examples/sfm3000werdo_expl.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include <sfm3000wedo.h>
+
+SFM3000wedo measflow(64);
+
+int offset = 32000; // Offset for the sensor
+float scale = 140.0; // Scale factor for Air and N2 is 140.0, O2 is 142.8
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+  delay(500); // let serial console settle
+
+
+  // initialize the sesnor
+  measflow.init();
+  Serial.println("Sensor initialized!");
+}
+
+void loop() {
+  int result = measflow.getvalue();
+
+  float Flow = ((float)result - offset) / scale;
+    
+  Serial.print("Flow: ");
+  if (result >= 0) {
+    Serial.print(" ");
+  }
+  Serial.print(Flow, 5);
+  Serial.print("\n");
+
+  delay(100);
+}
